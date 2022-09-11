abcbc812288c1355c5ade8011bd357cf611d02c8
Emma Han <3547964439@qq.com>
2022-09-11 19:06:27 +0800

    Added heroku config

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const bookingRouter = require('./routes/bookingRoute');
 
 const app = express();
 
+app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
